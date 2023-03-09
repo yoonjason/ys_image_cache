7fbfb71b0c32b43681bca931fb0df44d1b6d5c6c
Bradley.yoon <bradley.yoon@kakaocorp.com>
2023-03-09 15:02:36 +0900

    first commit

--- Package.swift
+++ Package.swift
@@ -10,17 +10,23 @@ let package = Package(
         .library(
             name: "ys_image_cache",
             targets: ["ys_image_cache"]),
+//        .library(
+//            name: "ys_networking",
+//            targets: ["ys_networking"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/yoonjason/ys_networking", from: "1.0.4")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ys_image_cache",
-            dependencies: []),
+            dependencies: [
+                .product(name: "ys_networking", package: "ys_networking")
+            ]),
         .testTarget(
             name: "ys_image_cacheTests",
             dependencies: ["ys_image_cache"]),
--- Sources/ys_image_cache/ys_image_cache.swift
+++ Sources/ys_image_cache/ys_image_cache.swift
@@ -1,6 +1,28 @@
+import Foundation
+import ys_networking
+
+
 public struct ys_image_cache {
     public private(set) var text = "Hello, World!"
 
     public init() {
     }
+}
+
+
+
+
+
+public class test {
+    public static let shared = test()
+    
+    
+    
+    public func getImage() {
+        let net  = NetworkManager.shared
+        net.test()
+    }
+    
+    
+    
 }
